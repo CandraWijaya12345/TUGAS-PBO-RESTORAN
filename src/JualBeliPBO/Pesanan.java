da4580057f5d704193bd10aaea29d33c5611cccc
candrawijaya06 <hacciwijaya06@gmail.com>
2023-04-18 10:49:44 +0800

    menambahkan pesanan, lihat pesanan, melihat menu

---
+++ src/JualBeliPBO/Pesanan.java
@@ -0,0 +1,20 @@
+package JualBeliPBO;
+
+import java.util.ArrayList;
+
+public class Pesanan {
+    private ArrayList<String> daftarPesanan;
+    private Restoran namaRestoran;
+    private Menu menuPesan;
+    private int jmlhPesanan;
+
+    public Pesanan(String namaRestoran, String menuPesan, double jmlhPesanan) {
+        this.namaRestoran = this.namaRestoran;
+        this.menuPesan = this.menuPesan;
+        this.jmlhPesanan = this.jmlhPesanan;
+    }
+
+    public void add(Pesanan pesanLagi) {
+
+    }
+}
